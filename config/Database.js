dcbc23dfd798cd3e5e9864ef156860a7a6150149
mila <mbakmilaa6@gmail.com>
2023-10-09 22:05:30 +0700

    update

--- config/Database.js
+++ config/Database.js
@@ -1,16 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  "railway", 
-  "root",
-  "jv2Ko94DFoyoz1CgVAJd",
-      {
-    host : "containers-us-west-51.railway.app",
-    port:"5777",
-    dialect: "mysql",
+const db = new Sequelize("railway", "root", "Jvpa4qYS1iYerGp6Ieu1", {
+  host: "containers-us-west-84.railway.app",
+  port: "6016",
+  dialect: "mysql",
 });
 
-coursedb.authenticate()
+coursedb
+  .authenticate()
   .then(() => console.log("Database connected"))
   .catch((error) => {
     console.log(`Unable to connect to database: ${error}`);
