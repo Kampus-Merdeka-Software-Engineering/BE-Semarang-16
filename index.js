1f086c5e3c1a805a69da53ebb3d4a6301669a731
RaniKusumaAfr <142371693+RaniKusumaAfr@users.noreply.github.com>
2023-10-10 14:53:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from "express";
 import CourseRoute from "./routes/CourseRoute.js";
-import TutorRoute from "./routes/TutorRoute.js";
-import FaqRoute from "./routes/FaqRoute.js";
 import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
@@ -11,7 +9,5 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/course", CourseRoute);
-app.use("/tutor", TutorRoute);
-app.use("/faq", FaqRoute);
 
 app.listen(PORT, "0.0.0.0", function () {});
